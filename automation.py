64b8cd03145cb01b0ed652072de182df5f83718b
shahidul-7 <shahidul.du34@gmail.com>
2024-07-11 19:47:33 +0600

    Now we can see how many products are for each company!

--- automation.py
+++ automation.py
@@ -8,4 +8,11 @@ product_per_suplier = {}
 
 for product_row in range(2, product_list.max_row + 1):
     suplier_names = product_list.cell(product_row, 4).value
-    print(product_row, suplier_names)
+    
+    if suplier_names in product_per_suplier:
+        current_num_product = product_per_suplier[suplier_names]
+        product_per_suplier[suplier_names] = current_num_product + 1
+    else:
+        product_per_suplier[suplier_names] = 1
+    
+print(product_per_suplier)
