af31a683384cf270781fb877d4933ab5e9850fba
shahidul-7 <shahidul.du34@gmail.com>
2024-07-11 17:59:21 +0600

    We can see all company now with counting

--- automation.py
+++ automation.py
@@ -1,1 +1,11 @@
-print("Hello World")
+import openpyxl
+
+inv_file = openpyxl.load_workbook("inventory.xlsx")
+
+product_list = inv_file["Sheet1"]
+
+product_per_suplier = {}
+
+for product_row in range(2, product_list.max_row + 1):
+    suplier_names = product_list.cell(product_row, 4).value
+    print(product_row, suplier_names)
